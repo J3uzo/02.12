42d6402cbb341103605e74e2e85221270e71df05
Rokima <55d55r77@gmail.com>
2025-11-30 23:10:46 +0300

    add auth_router in app

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import uvicorn
 from fastapi import FastAPI
 from app.api.sample import router as sample_router
+from app.api.auth import router as auth_router
 
 app = FastAPI(title="individual_project_template", version="0.0.1")
 
 app.include_router(sample_router)
+app.include_router(auth_router)
 
 if __name__ == "__main__":
-    uvicorn.run(app="app")
+    uvicorn.run(app=app)
